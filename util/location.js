b8e5e0f4d43865c7ffcb27b84b6013289b4bbebd
Samantha Raymond <sam@apps-craft.com>
2024-08-22 10:39:56 +0100

    Update location.js

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyAJqB7MWJ9zGAFZFfJV_tyry7FcyNeehEg";
+const GOOGLE_API_KEY = "";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
